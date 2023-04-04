1b34b4de5c03bc32d6af627ffeed6f9e650735c5
pendulum <rs.pendulum@gmail.com>
2023-04-04 12:46:19 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using EasyModbus;
+using TrafficGen.WebSocket;
+
+namespace TrafficGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Server = new WSServer();
+            Server.Start();
+
+            var server = new ModbusServer();
+
+            server.Port = 502;
+            server.Listen();
+
+            var client = new EasyModbus.ModbusClient("127.0.0.10", 502);
+            
+            client.Connect();
+            short value = 0;
+            while (true)
+            {
+                
+                client.ReadHoldingRegisters(0, 2);
+
+                server.holdingRegisters.localArray[1] = value;
+                server.holdingRegisters.localArray[2] = value;
+                server.discreteInputs.localArray[1] = true;
+                server.inputRegisters.localArray[1] = 228;
+                server.coils.localArray[1] = true;
+                server.coils.localArray[2] = true;
+                client.WriteSingleRegister(0, value);
+                client.ReadHoldingRegisters(0, 2);
+                client.ReadInputRegisters(0, 2);
+                client.ReadDiscreteInputs(0, 2);
+                client.ReadCoils(0,2);
+                //var collection = client.ReadHoldingRegisters(0, 10);
+                //foreach(var item in collection)
+                //{
+                //    Console.Write(item);
+                //}
+                //Console.WriteLine();
+                Thread.Sleep(1000);
+                value = Convert.ToInt16(value + 1);
+            }
+        }
+    }
+}
---
+++ WebSocket/WSServer.cs
@@ -0,0 +1,84 @@
+﻿using SuperSocket.SocketBase;
+using SuperWebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TrafficGen.WebSocket
+{
+    public class WSServer
+    {
+        private WebSocketServer Server;
+
+        private Dictionary<string, Thread> SessionsSendingData;
+
+        private static object syncObject = new object();
+
+        public WSServer()
+        {
+            Server = new WebSocketServer();
+            Server.Setup(8080);
+
+            Server.NewSessionConnected += OnNewSessionConnected;
+            Server.NewMessageReceived += OnNewMessageReceived;
+            Server.SessionClosed += OnSessionClosed;
+
+            SessionsSendingData= new Dictionary<string, Thread>();
+        }
+
+        private void MainSendingData(object o)
+        {
+            if (o is WebSocketSession session)
+            {
+
+                while (true)
+                {
+                    lock(syncObject)
+                    {
+                        session.Send("Hey you");
+                    }
+                    
+                    Thread.Sleep(1000);
+                }
+
+            }
+        }
+
+        public void Start()
+        {
+            Server.Start();
+        }
+
+        public void Stop()
+        {
+            Server.Stop();
+        }
+
+        private void OnNewSessionConnected(WebSocketSession session)
+        {
+            SessionsSendingData.Add(session.SessionID, new Thread(new ParameterizedThreadStart(MainSendingData)));
+            SessionsSendingData[session.SessionID].Start(session);
+
+            Console.WriteLine($"Session {session.SessionID} connected");
+        }
+
+        private void OnNewMessageReceived(WebSocketSession session, string message)
+        {
+            Console.WriteLine($"Received message: {message}");
+        }
+
+        private void OnSessionClosed(WebSocketSession session, CloseReason reason)
+        {
+            SessionsSendingData.Remove(session.SessionID);
+
+            Console.WriteLine($"Session {session.SessionID} closed: {reason}");
+            Console.WriteLine("Clients: ");
+
+            foreach (var item in SessionsSendingData)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+    }
+}
